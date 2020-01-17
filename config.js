e31258d0cf27af0117d3a5856410b29f4cd3d865
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-01-17 10:23:48 -0500

    Change wallet type hint to VerifiablePresentation.

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const MEDIATOR = 'https://beta.authn.io/mediator' + '?origin=' +
+  encodeURIComponent(window.location.origin);
+
+const WALLET_LOCATION = 'https://digitalbazaar.github.io/chapi-demo-wallet/';
