0a692f8f175861b947a5b4e62c5745888bfbe95f
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-01-17 00:20:41 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Credential Handler API Demo Wallet
+
+> [Credential Handler API](https://w3c-ccg.github.io/credential-handler-api/) (CHAPI) demo wallet app
+
+## Table of Contents
+
+- [Background](#background)
+- [Usage](#usage)
+- [Install](#install)
+- [Security](#security)
+- [Contribute](#contribute)
+- [Commercial Support](#commercial-support)
+- [License](#license)
+
+Visit https://digitalbazaar.github.io/credential-handler-polyfill/demo/ 
+for an idea of what the basic handler UI looks like.
+
+## Background
+
+## Usage
+
+## Security
+
+See https://github.com/digitalbazaar/credential-handler-polyfill#security
+
+## Contribute
+
+See [the contribute file](https://github.com/digitalbazaar/bedrock/blob/master/CONTRIBUTING.md)!
+
+PRs accepted.
+
+Note: If editing the README, please conform to the
+[standard-readme](https://github.com/RichardLitt/standard-readme) specification.
+
+## Commercial Support
+
+Commercial support for this library is available upon request from
+Digital Bazaar: support@digitalbazaar.com
+
+## License
+
+[New BSD License (3-clause)](LICENSE) © 2020 Digital Bazaar
---
+++ install-wallet.js
@@ -0,0 +1,40 @@
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+/* global navigator, window, document */
+'use strict';
+
+const workerUrl = 'https://digitalbazaar.github.io/chapi-demo-wallet/worker.html'
+
+const MEDIATOR = 'https://beta.authn.io/mediator' + '?origin=' +
+  encodeURIComponent(window.location.origin);
+
+async function installHandler() {
+  console.log('Loading polyfill...');
+
+  try {
+    await credentialHandlerPolyfill.loadOnce(MEDIATOR);
+  } catch(e) {
+    console.error('Error in loadOnce:', e);
+  }
+
+  console.log('Polyfill loaded.');
+  // document.getElementById('loadingText').innerHTML = 'Polyfill loaded.';
+
+  const registration = await WebCredentialHandler
+    .installHandler({url: workerUrl})
+
+  await registration.credentialManager.hints.set(
+    'test', {
+      name: 'TestUser',
+      enabledTypes: ['VerifiablePresentation', 'VerifiableCredential', 'AlumniCredential']
+    });
+}
+
+function ready(fn) {
+  if (document.readyState !== 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
