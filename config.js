96c6ce7995650fb718fb87bae042aff42a5acac4
Ganesh Annan <gannan@digitalbazaar.com>
2022-06-04 16:09:19 -0400

    Prep demo for updating handler polyfill.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
  * wallets, etc).
  */
 
-const MEDIATOR = 'https://authn.io/mediator' + '?origin=' +
+const MEDIATOR = 'https://authn.localhost:33443/mediator' + '?origin=' +
   encodeURIComponent(window.location.origin);
 
 const WALLET_LOCATION = window.location.origin + '/';
